72bbd76dbd45ba26b132b67b648a6733661d9e5c
Christina S. Wang <christina.s.wang@vanderbilt.edu>
2016-10-15 22:54:51 -0500

    changes

--- agents/myagent.py
+++ agents/myagent.py
@@ -5,6 +5,7 @@
 
 from agent import Agent
 
+
 from collections import deque
 import copy
 from sets import Set
@@ -53,6 +54,11 @@ class MyAgent(Agent):
             nbrs.add(i)
             nodeNeighbors.append(nbrs)
 
+        #testing Christina
+        print "testing"
+        for p in nodeNeighbors: print p
+        print "End of 50 nodes"
+
         # initialize the "Frontier" 
         frontier = Frontier()
 
@@ -64,57 +70,26 @@ class MyAgent(Agent):
 
         best = tuple(x)
         bestVal = 0
+        
+        #testing Christina
+        print "This is what x starts as"
+        for p in x: print p
             
         ### your code goes here ###
         # [ NOTE: fill in where necessary ]
-                
+
+        #testing what expanding node 1 does
+        print "testing result of expanding node 1" 
+        nodes=self.expand(1);
+        for p in nodes: print p
+            
+            
         while not frontier.empty():
+            for i in range(numNodes):
+            
+        
 
-            # take the front element from the frontier
-            x = frontier.pop()
-
-            # manage frontier and branch-and-bound search
-            # prune nodes (and subtrees) as needed
-
-                    while not frontier.empty():
-
-            # take the front element from the frontier
-            x = frontier.pop()
-
-            # manage frontier and branch-and-bound search
-            # prune nodes (and subtrees) as needed
-
-
-        #using A* search--Christina
-        frontier = util.PriorityQueue()
-        frontier.push([problem.getStartState()], 0)
-        came_from = {}
-        cost_so_far = {}
-        #came_from[problem.getStartState()] = None
-        cost_so_far[problem.getStartState()] = 0
-        solution = []
-
-        while not frontier.isEmpty():
-            current = frontier.pop()
-            print 'current: ', current
-            if problem.isGoalState(current[0]):
-                # return solution
-                here = current
-                solution.append(here[1])
-                # print 'CAME_FROM: ', came_from
-                print here, ' came from ', came_from[here[0]]
-                while came_from[here[0]][0] != problem.getStartState():
-                    here = came_from[here[0]]
-                    solution.append(here[1])
-                # print 'solution: ', solution
-                return list(reversed(solution))
-            for child in problem.getSuccessors(current[0]):
-                new_cost = cost_so_far[current[0]] + child[2]
-                if child[0] not in cost_so_far or new_cost < cost_so_far[child[0]]:
-                    cost_so_far[child[0]] = new_cost
-                    priority = new_cost + heuristic(child[0], problem)
-                    frontier.push(child, priority)
-                    came_from[child[0]] = current
+           
 
         ### end your code ###
 
@@ -134,13 +109,16 @@ class MyAgent(Agent):
         nodes = []
 
         ### your code goes here  ####
+
+        #I am not sure why this expand method is necessary when we can just use the getNeighbors from the networks.py file
+        #I can't do that here since networks is not a param, but I can do it in the above section
+        #network.getNeighbors(x); 
         
-        nodes=self.Network.getNeighbors(); #Christina
-        
+    
         
             ### end your code  ###
 
-            return nodes
+        return nodes
 
     def eval(self, nodeNeighbors, x):
         """
